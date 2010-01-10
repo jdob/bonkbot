3f5f0bcce5edee20e4b7dee30400697bed9f184b
Jason Dobies <jason.dobies@redhat.com>
2010-01-10 16:00:08 -0500

    Added stock quote plugin

--- plugins.py
+++ plugins.py
@@ -2,5 +2,6 @@ import basic
 import insult
 import weather
 import karma
+import quote
 
-PLUGINS = (basic.echo, insult.insult, weather.weather, karma.karma)
+PLUGINS = (basic.echo, insult.insult, weather.weather, karma.karma, quote.quote)
---
+++ quote.py
@@ -0,0 +1,20 @@
+from config import *
+import urllib
+
+def quote(irc, data):
+    if data.find('!%s quote' % NICK) != -1:
+        args = data.split()
+
+        if len(args) > args.index('quote') + 1:
+            channel = args[2]
+            symbol = args[args.index('quote') + 1]
+
+            price = lookup(symbol, 'l1')
+            change = lookup(symbol, 'c1')
+
+            irc.send('PRIVMSG ' + channel + ' :' + symbol + ' - Price: $' + price + ', Change: $' + change + '\r\n')
+
+def lookup(symbol, f):
+    url = 'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s' % (symbol, f)
+    value = urllib.urlopen(url).read().strip().strip('"')
+    return value
