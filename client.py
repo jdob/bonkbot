c69b27a10bb374c80a7c8c14a04655ebe70db50b
Jason Dobies <jason.dobies@redhat.com>
2010-01-11 16:50:46 -0500

    Added ability to run the listening loop as a thread so the bot can be run in an interactive shell and the IRC connection dorked with.

--- client.py
+++ client.py
@@ -1,29 +1,13 @@
 from config import *
+from irc_utils import msg
 import plugins
 import socket
 import sys
 import traceback
+import thread
 
-if __name__ == "__main__":
-    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    print("Connecting to %s on port %s..." % (HOST, PORT))
-
-    irc.connect((HOST, PORT))
-
-    print('Connected')
-    print(irc.recv(4096))
-    print('Received initial data')
-
-    print('Sending initial configuration...')
-    irc.send('NICK %s\r\n' % (NICK))
-    irc.send('USER %s 0 * :%s\r\n' %(NICK, NAME))
-    irc.send('JOIN %s\r\n' % (CHANNELS))
-
-    print('Sent initial configuration:')
-    print('  NICK: %s' % (NICK))
-    print('  USER: %s' % (NAME))
-    print('  JOIN: %s' % (CHANNELS))
+def listen(irc):
+    ''' Begins a loop to handle all incoming data from the IRC server. '''
 
     while True:
         data = irc.recv(4096)
@@ -49,3 +33,45 @@ if __name__ == "__main__":
         if data.find('!%s quit' % NICK) != -1:
             irc.send('QUIT :Fine, I\'ll leave... \r\n')
             sys.exit()
+
+def connect():
+    ''' Creates and returns a connection to the IRC server, using properies imported from config. '''
+
+    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print("Connecting to %s on port %s..." % (HOST, PORT))
+
+    irc.connect((HOST, PORT))
+
+    print('Connected')
+    print(irc.recv(4096))
+    print('Received initial data')
+
+    print('Sending initial configuration...')
+    irc.send('NICK %s\r\n' % (NICK))
+    irc.send('USER %s 0 * :%s\r\n' %(NICK, NAME))
+    irc.send('JOIN %s\r\n' % (CHANNELS))
+
+    print('Sent initial configuration:')
+    print('  NICK: %s' % (NICK))
+    print('  USER: %s' % (NAME))
+    print('  JOIN: %s' % (CHANNELS))
+
+    return irc
+
+def start():
+    ''' Connects and starts a new thread listening to incoming server data. The connection
+        to the IRC server is returned. '''
+
+    irc = connect()
+    thread.start_new_thread(listen, (irc,))
+    return irc
+
+def speak(irc, channel, message):
+    ''' Sends the given message to a channel on the IRC server. '''
+
+    msg(irc, channel, message)
+
+if __name__ == "__main__":
+    irc = connect()
+    listen(irc)
