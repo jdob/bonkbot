cc2aa73b994b6fd2cf76cc9d051d55a472cf1c78
Jason Dobies <jason.dobies@redhat.com>
2010-01-10 19:24:06 -0500

    Refactored so I can better test it eventually (better late than never)

--- twitter.py
+++ twitter.py
@@ -9,11 +9,20 @@ def twitter(irc, data):
         if len(args) > args.index('twitter') + 1:
             channel = args[2]
             user = args[args.index('twitter') + 1]
-            url = 'http://twitter.com/statuses/user_timeline.rss?screen_name=%s' % user
-            dom = minidom.parse(urllib.urlopen(url))
-            tweets = dom.getElementsByTagName('item')
 
-            if len(tweets) > 0:
-                latest = tweets.item(0).getElementsByTagName('title')[0].childNodes[0].data
-                latest = latest.encode('UTF-8') # Needed since a coworker regularly tweets in German  :)
-                irc.send('PRIVMSG ' + channel + ' :"' + latest + '"\r\n')
+            tweet = __findTweet(user)
+
+            if tweet is not None:
+                irc.send('PRIVMSG ' + channel + ' :"' + tweet + '"\r\n')
+
+def __findTweet(user):
+    url = 'http://twitter.com/statuses/user_timeline.rss?screen_name=%s' % user
+    dom = minidom.parse(urllib.urlopen(url))
+    tweets = dom.getElementsByTagName('item')
+
+    if len(tweets) > 0:
+        latest = tweets.item(0).getElementsByTagName('title')[0].childNodes[0].data
+        latest = latest.encode('UTF-8') # Needed since a coworker regularly tweets in German  :)
+        return latest
+    else:
+        return None
