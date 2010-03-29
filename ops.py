bf89b1d0960104504b83ded1452bd4903cfbdffb
Jason Dobies <jason.dobies@redhat.com>
2010-03-29 09:19:21 -0400

    Code style consistency.

--- ops.py
+++ ops.py
@@ -1,5 +1,7 @@
 def give(message):
     if message.data.find('JOIN') != -1:
-        newOp = message.author()
-        if newOp != message.config['nick']:
+        new_op = message.author()
+
+        # Don't try to op yourself
+        if new_op != message.config['nick']:
             message.irc.send('MODE ' + message.channel()[1:] + ' +o ' + message.author() + '\r\n')
