3785f132914cf93105517fd4984e1640279fe731
Jason Dobies <jason.dobies@redhat.com>
2010-04-20 15:16:16 -0400

    Made karma handling smarter

--- karma.py
+++ karma.py
@@ -1,5 +1,8 @@
 import pickle
+import re
 import os
+
+VALID_NAME = r'^([A-Za-z0-9_-]|\[|\])*$'
 
 KARMA = {}
 
@@ -25,34 +28,31 @@ def karma(message):
     __save(message.config['karmaFile'])
 
 def __add(message):
-    for s in message.data.split():
-        index = s.find('++')
-        if index > 0:
-            name = s[:index]
-
-            if name[0] == ':':
-                name = name[1:]
-
-            if KARMA.has_key(name):
-                KARMA[name] = int(KARMA[name]) + 1
-            else:
-                KARMA[name] = 1
-            __printKarma(message, name)
+    __assign_karma(message, '++', 1)
 
 def __remove(message):
+    __assign_karma(message, '--', -1)
+
+def __assign_karma(message, flag, step):
     for s in message.data.split():
-        index = s.find('--')
-        if index > 0:
-            name = s[:index]
+        if not s.endswith(flag):
+            continue
 
-            if name[0] == ':':
-                name = name[1:]
+        name = s[0:len(s) - 2]
+        
+        if name[0] == ':':
+            name = name[1:]
 
-            if KARMA.has_key(name):
-                KARMA[name] = int(KARMA[name]) - 1
-            else:
-                KARMA[name] = -1
-            __printKarma(message, name)
+        if not re.match(VALID_NAME, name):
+            print('Not a valid name [%s]' % name)
+            continue
+
+        if KARMA.has_key(name):
+            KARMA[name] = int(KARMA[name]) + step
+        else:
+            KARMA[name] = step
+
+        __printKarma(message, name)
 
 def __list(message):
     if len(KARMA) == 0:
