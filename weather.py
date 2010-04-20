4452e094ed7237f47e12f0abc9772d77e1cd867f
Jason Dobies <jason.dobies@redhat.com>
2010-04-20 10:07:30 -0400

    Added three day forecast to weather plugin.

--- weather.py
+++ weather.py
@@ -4,7 +4,10 @@ import urllib
 
 WEATHER_URL = 'http://xml.weather.yahoo.com/forecastrss?p=%s'
 WEATHER_NS = 'http://xml.weather.yahoo.com/ns/rss/1.0'
-WEATHER_FORMAT = '%s -> Temp: %sF, Condition: %s'
+
+WEATHER_FORMAT = 'Yahoo! Weather for %s, %s'
+CURRENT_FORMAT = 'Current -> Temp: %sF, Condition: %s'
+FORECAST_FORMAT = '%s -> Low: %sF, High: %sF, Condition: %s'
 
 @command('weather')
 def weather(message):
@@ -13,15 +16,29 @@ def weather(message):
     args = message.data.split()
 
     if len(args) > args.index('weather') + 1:
-        zipCode = args[args.index('weather') + 1]
-        message.reply(__weather_as_string(zipCode))
+        zip_code = args[args.index('weather') + 1]
+        msgs = __weather_as_strings(zip_code)
+        for m in msgs:
+            message.reply(m)
 
-def __weather_as_string(zipCode):
-    url = WEATHER_URL % zipCode
+def __weather_as_strings(zip_code):
+    url = WEATHER_URL % zip_code
     dom = minidom.parse(urllib.urlopen(url))
-    condition = dom.getElementsByTagNameNS(WEATHER_NS, 'condition')[0]
     
-    return WEATHER_FORMAT % (dom.getElementsByTagName('title')[0].firstChild.data, condition.getAttribute("temp"), condition.getAttribute('text'))
+    location_node = dom.getElementsByTagNameNS(WEATHER_NS, 'location')[0]
+    location = WEATHER_FORMAT % (location_node.getAttribute('city'), location_node.getAttribute('region'))
+
+    condition_node = dom.getElementsByTagNameNS(WEATHER_NS, 'condition')[0]
+    conditions = CURRENT_FORMAT % (condition_node.getAttribute("temp"), condition_node.getAttribute('text'))
+
+    weather = [location, conditions]
+
+    forecast_nodes = dom.getElementsByTagNameNS(WEATHER_NS, 'forecast')
+    for node in forecast_nodes:
+        forecast = FORECAST_FORMAT % (node.getAttribute('day'), node.getAttribute('low'), node.getAttribute('high'), node.getAttribute('text'))
+        weather.append(forecast)
+        
+    return weather
 
 if __name__ == '__main__':
     print(weatherAsString('08056'))
